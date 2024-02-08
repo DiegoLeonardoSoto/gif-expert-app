c4226da66735d4e424b7f4c2a220c184df372e6d
Diego Soto <88800678+DiegoLeonardoSoto@users.noreply.github.com>
2024-02-08 11:23:25 -0300

    finish app

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -5,10 +5,12 @@ import { useFetchGifs } from '../hooks/useFetchGifs'
 const GifGrid = ({ category }) => {
   const { images, isLoading } = useFetchGifs(category)
 
-  console.log(isLoading)
   return (
     <>
       <h3>{category}</h3>
+
+      {isLoading && <h2>Loading...</h2>}
+
       <div className="card-grid">
         {images.map((image) => {
           return <GifItem key={image.id} {...image} />
